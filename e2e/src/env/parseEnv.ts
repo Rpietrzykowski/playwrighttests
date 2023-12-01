6bbf8bead727414b97af360494158e8f0a75f84e
Robert Pietrzykowski <rpietrzykowski@me.com>
2023-12-01 18:32:01 +0100

    PoC of Playwright configuration project

---
+++ e2e/src/env/parseEnv.ts
@@ -0,0 +1,9 @@
+export const env = (key: string): string => {
+    const value = process.env[key];
+
+    if(!value){
+        throw Error(`No enviroment variable found for ${key}`);
+    }
+
+    return value;
+}
